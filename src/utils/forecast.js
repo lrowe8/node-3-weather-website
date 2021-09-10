b127a1c653a66976bc873bf98155eb86480b50b2
Laremy <ssbrllc@gmail.com>
2021-09-10 12:41:58 -0600

    Updated output to include windspeed and direction

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,12 +4,14 @@ const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=82074549d45d2a41eead6521edb32d87&query=' + latitude + ',' + longitude + '&units=f'
 
     request({ url: url, json: true }, (error, response, body) => {
+        console.log(body)
+
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out.  It feels like ' + body.current.feelslike + ' degrees out.')
+            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out.  It feels like ' + body.current.feelslike + ' degrees out.\nThe wind is blowing ' + body.current.wind_dir + ' at ' + body.current.wind_speed + ' MPH.')
         }
     })
 }
