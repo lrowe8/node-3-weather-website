a6b7dd2b9a945928ab3a5c99bc69173387f52950
Laremy <ssbrllc@gmail.com>
2021-09-10 08:14:08 -0600

    Removed un-needed commnets

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,3 @@
-console.log('Client side javascript file is loaded!')
-
-/*fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
-
-fetch('http://192.168.2.9:3000/weather?address=boston').then((response) => {
-    response.json().then((data) => {
-        if(data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.forecast)
-            console.log(data.location)
-        }
-    })
-})*/
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const locationSel = document.querySelector('#location')
